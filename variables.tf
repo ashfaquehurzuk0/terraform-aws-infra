dbe7bb5382d59f7649ed23fcdfe234c3c8d34db2
ashfaquehurzuk0 <ec2-user@ip-172-31-9-20.ap-south-1.compute.internal>
2026-05-09 08:44:52 +0000

    Initial Terraform AWS infrastructure - VPC, EC2, Security Group

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0f58b397bc5c1f2e8"  # Amazon Linux 2 in Mumbai
+}
---
+++ vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "devops-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "main-igw" }
+}
